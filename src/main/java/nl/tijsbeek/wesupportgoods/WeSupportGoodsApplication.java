e072e5404b7fe18a1220cd15c3fc2922f8041230
Tais993 <tijs@familiebeek.eu>
2022-11-09 23:45:42 +0100

    Added a working JavaFX GUI

--- src/main/java/nl/tijsbeek/wesupportgoods/WeSupportGoodsApplication.java
+++ src/main/java/nl/tijsbeek/wesupportgoods/WeSupportGoodsApplication.java
@@ -1,13 +1,8 @@
 package nl.tijsbeek.wesupportgoods;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class WeSupportGoodsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeSupportGoodsApplication.class, args);
+        JavaFxApplicationSupport.main(args);
     }
-
 }
