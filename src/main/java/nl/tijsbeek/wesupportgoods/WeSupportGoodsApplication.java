b1a7b1dd2ad3f0a59bb0ba6f756ceba097aa44c2
Tais993 <tijs@familiebeek.eu>
2022-11-09 14:46:54 +0100

    Initial set-up

---
+++ src/main/java/nl/tijsbeek/wesupportgoods/WeSupportGoodsApplication.java
@@ -0,0 +1,13 @@
+package nl.tijsbeek.wesupportgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeSupportGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeSupportGoodsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/tijsbeek/wesupportgoods/WeSupportGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tijsbeek.wesupportgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeSupportGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
